ac4439813b1bcf06ab3a72343ae3ddbf57e7cc75
Yoann Rodière <yoann@hibernate.org>
2019-05-23 10:02:54 +0200

    Test loading of Hibernate Search services from Hibernate ORM

--- main/src/main/java/module-info.java
+++ main/src/main/java/module-info.java
@@ -1,5 +1,20 @@
 module org.hibernate.sandbox.java.main {
 	requires org.hibernate.sandbox.java.service.consumer;
 
+	requires org.hibernate.orm.core;
+	requires org.hibernate.search.mapper.orm;
+
+	/*
+	 * Apparently this is necessary in order to use ORM,
+	 * otherwise we get compile-time errors for some modules such as "cannot access javax.naming.Referenceable",
+	 * or runtime errors for other modules such as "java.lang.NoClassDefFoundError: javax/xml/bind/JAXBException".
+	 */
+	requires java.naming;
+	requires java.sql;
+	requires java.xml.bind;
+	requires net.bytebuddy;
+	// Strangely, this is not required
+	//requires h2;
+
 	exports org.hibernate.sandbox.java.main;
 }
--- main/src/main/java/org/hibernate/sandbox/java/main/Main.java
+++ main/src/main/java/org/hibernate/sandbox/java/main/Main.java
@@ -1,9 +1,20 @@
 package org.hibernate.sandbox.java.main;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.integrator.spi.IntegratorService;
 import org.hibernate.sandbox.java.service.consumer.MyService;
 import org.hibernate.sandbox.java.service.consumer.ServiceConsumer;
+import org.hibernate.search.mapper.orm.impl.HibernateSearchContextService;
+import org.hibernate.service.spi.ServiceBinding;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 public class Main {
 
@@ -35,9 +46,76 @@ public class Main {
 
 		System.out.println();
 		System.out.println();
+		System.out.println( "Booting Hibernate ORM service registry..." );
+		List<?> hibernateOrmServicesFromHibernateOrmServiceRegistry = startHibernateOrmServiceRegistryAndGetServices();
+		System.out.flush();
+		System.out.println( "Finished booting Hibernate ORM service registry." );
+		System.out.println();
+		System.out.println();
+		System.out.println( "===== RESULT:" );
+		System.out.println( "Hibernate services loaded from Hibernate ORM module by bootstrapping a service registry only: " );
+		System.out.println( hibernateOrmServicesFromHibernateOrmServiceRegistry );
+
+		System.out.println();
+		System.out.println();
+		System.out.println( "Booting Hibernate ORM session factory..." );
+		List<?> hibernateOrmServicesFromHibernateOrmSessionFactory = startHibernateOrmSessionFactoryAndGetServices();
+		System.out.flush();
+		System.out.println( "Finished booting Hibernate ORM session factory." );
+		System.out.println();
+		System.out.println();
+		System.out.println( "===== RESULT:" );
+		System.out.println( "Hibernate services loaded from Hibernate ORM module by bootstrapping a whole session factory: " );
+		System.out.println( hibernateOrmServicesFromHibernateOrmSessionFactory );
+
+		System.out.println();
+		System.out.println();
 		System.out.println( "======================================================" );
 		System.out.println( "                   ENDING EXECUTION                   " );
 		System.out.println( "======================================================" );
+	}
+
+	private static List<?> startHibernateOrmServiceRegistryAndGetServices() {
+		List<Object> loaded = new ArrayList<>();
+
+		try ( ServiceRegistryImplementor serviceRegistry = createServiceRegistry() ) {
+			collectOrmServices( loaded, serviceRegistry );
+		}
+
+		return loaded;
+	}
+
+	private static List<?> startHibernateOrmSessionFactoryAndGetServices() {
+		List<Object> loaded = new ArrayList<>();
+		MetadataSources metadataSources = new MetadataSources( createServiceRegistry() );
+		Metadata metadata = metadataSources.buildMetadata();
+		SessionFactoryBuilder sessionFactoryBuilder = metadata.getSessionFactoryBuilder();
+
+		try ( SessionFactoryImplementor sessionFactory = (SessionFactoryImplementor) sessionFactoryBuilder.build() ) {
+			ServiceRegistryImplementor serviceRegistry = sessionFactory.getServiceRegistry();
+			collectOrmServices( loaded, serviceRegistry );
+		}
+
+		return loaded;
+	}
+
+	private static ServiceRegistryImplementor createServiceRegistry() {
+		StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+		registryBuilder.applySetting( AvailableSettings.DIALECT, "org.hibernate.dialect.H2Dialect" );
+		return (ServiceRegistryImplementor) registryBuilder.build();
+	}
+
+	private static void collectOrmServices(List<Object> loaded, ServiceRegistryImplementor serviceRegistry) {
+		IntegratorService integratorService = serviceRegistry.getService( IntegratorService.class );
+		if ( integratorService != null ) {
+			integratorService.getIntegrators().forEach( loaded::add );
+		}
+
+		ServiceBinding<HibernateSearchContextService> searchContextServiceBinding =
+				serviceRegistry.locateServiceBinding( HibernateSearchContextService.class );
+		if ( searchContextServiceBinding != null ) {
+			loaded.add( searchContextServiceBinding.getService() );
+		}
 	}
 
 }
